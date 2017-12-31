4e27ed2fd906b4b6c71ca84e709565c9c471985b
Chris <chris@usablehq.com>
2017-12-31 14:31:54 +0000

    Rune init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import "./calligraphy.css";
 import Rune from "rune.js";
 
-console.log("calligraphy");
+class Calligraphy {
+  constructor() {
+    this.paper = new Rune({
+      container: "#paper",
+      width: 800,
+      height: 600,
+      debug: true
+    });
+    this.test();
+  }
+  test() {
+    this.paper
+      .path(250, 140) //move to point
+      .curveTo(0, 0, 50, -100, 100, 0) //x,y, ctrlx1,ctrly1, ctrlx2,ctrly2
+      .curveTo(100, 0, 150, 100, 200, 0) //x,y, ctrlx1,ctrly1, ctrlx2,ctrly2
+      .curveTo(200, 0, 250, -100, 300, 0) //x,y, ctrlx1,ctrly1, ctrlx2,ctrly2
+      .lineTo(300, 200) //x,y
+      .lineTo(0, 200) //x,y
+      .closePath(); //end
+
+    this.paper.draw();
+  }
+}
+
+const cal = new Calligraphy();
