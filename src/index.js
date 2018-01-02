89a02aae59dab7b3f48dba36b8731a249ca26c9e
Chris <chris@usablehq.com>
2018-01-02 07:20:09 +0000

    Using data for position

--- src/index.js
+++ src/index.js
@@ -7,27 +7,23 @@ class Calligraphy {
       container: "#paper",
       width: 800,
       height: 600,
-      debug: false
-    });
-    this.paper.grid({
-      x: 0,
-      y: 0,
-      width: 800,
-      height: 600,
-      columns: 40,
-      rows: 30
+      debug: true
     });
     this.test();
   }
   test() {
     const stroke = {
-      x1: 200, //from
-      y1: 100,
-      x2: 0, //to
-      y2: 400,
       type: "outercurve",
+      posX: 100,
+      posY: 100,
+      x1: 0, //from
+      y1: 0,
+      x2: 0, //to - relative to start
+      y2: 400,
       angle: 30
     };
+
+    stroke.curve = { x1: 330, y1: stroke.y1, x2: 330, y2: stroke.y2 };
 
     /*
         distance between edges = pressure
@@ -43,10 +39,11 @@ class Calligraphy {
     const n2 = { x1: 350, y1: 400, x2: 350, y2: 0 };
 
     this.paper
-      .path(stroke.x1, stroke.y1) //move to point
+      .path(stroke.posX, stroke.posY) //move to point
+      .moveTo(stroke.x1, stroke.y1)
       .curveTo(n1.x1, n1.y1, n1.x2, n1.y2, stroke.x2, stroke.y2) //ctrlx1,ctrly1, ctrlx2,ctrly2, x,y
-      .curveTo(n2.x1, n2.y1, n2.x2, n2.y2, 0, 0) //ctrlx1,ctrly1, ctrlx2,ctrly2, x,y
-      .lineTo(0, 400)
+      .curveTo(n2.x1, n2.y1, n2.x2, n2.y2, stroke.x1, stroke.y1) //ctrlx1,ctrly1, ctrlx2,ctrly2, x,y
+      .lineTo(stroke.x2, stroke.y2)
       .fill(0)
       .closePath(); //end
 
